5f2afb4ff08fc362fa72bf7387cf863f1b12ca48
Kenneth Reitz <me@kennethreitz.com>
2013-09-30 20:51:56 -0400

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+"""
+Flask-WebSocket
+-------------
+
+Elegant WebSockets for your Flask app
+"""
+from setuptools import setup
+
+
+setup(
+    name='Flask-WebSocket',
+    version='0.1',
+    url='https://github.com/kennethreitz/flask-websocket',
+    license='See License',
+    author='Kenneth Reitz',
+    author_email='_@kennethreitz.com',
+    description='Elegant WebSockets for your Flask app.',
+    long_description=__doc__,
+    py_modules=['flask_websocket'],
+    zip_safe=False,
+    include_package_data=True,
+    platforms='any',
+    install_requires=[
+        'Flask',
+        'gevent'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
