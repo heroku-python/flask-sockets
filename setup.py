2b414eaad5bea3c6b2c8a8e193450eb7feecef9c
Alexander Oblovatniy <oblovatniy@gmail.com>
2016-05-01 17:13:49 +0300

    Add support of Flask blueprints

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Sockets',
-    version='0.2.0',
+    version='0.2.1',
     url='https://github.com/kennethreitz/flask-sockets',
     license='See License',
     author='Kenneth Reitz',
