e9b4779c8dc570dd3b956f24d7e4237cc0164207
Uwe L. Korn <xhochy@users.noreply.github.com>
2019-11-25 17:23:02 +0100

    Correct license in setup.py
    LICENSE is MIT, but setup.py listed BSD.

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     classifiers=[
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
+        'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
